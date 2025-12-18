68ee00c4b9c550663789007fb8331c3feece7a1c
Jonathan Lindqivst <jonathan.lindqivst@consid.se>
2025-12-18 09:20:41 +0100

    first commit

---
+++ website/astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import { loadEnv } from "payload/node";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+loadEnv();
+
+export default defineConfig({
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
+});
