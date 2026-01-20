8c63ecada240088c9c5f17942821a8af7204bede
Jonathan Lindqivst <jonathan.lindqivst@consid.se>
2026-01-20 08:41:25 +0100

    update shortinfo

--- website/src/components/shortInfo/shortInfoCarousel.jsx
+++ website/src/components/shortInfo/shortInfoCarousel.jsx
@@ -12,17 +12,18 @@ const ShortInfoCarousel = ({ items }) => {
         const iconPath = `../../icons/${item.icon}.astro`;
         // const IconComponent = iconModules[iconPath]?.default;
         return (
-          <li key={item.id} className="max-w-80">
-            <SwiperSlide className="flex flex-col items-center gap-4 text-center">
-              {/* {IconComponent && <IconComponent />}*/}
-              <Text as="h2" font="fontHeadings" weight="black" margin={false}>
-                {item.title}
-              </Text>
-              <Text as="p" font="fontBody" weight="default">
-                {item.description}
-              </Text>
-            </SwiperSlide>
-          </li>
+          <SwiperSlide
+            key={item.id}
+            className="flex flex-col items-center gap-4 text-center"
+          >
+            {/* {IconComponent && <IconComponent />}*/}
+            <Text as="h2" font="fontHeadings" weight="black" margin={false}>
+              {item.title}
+            </Text>
+            <Text as="p" font="fontBody" weight="default">
+              {item.description}
+            </Text>
+          </SwiperSlide>
         );
       })}
     </Carousel>
